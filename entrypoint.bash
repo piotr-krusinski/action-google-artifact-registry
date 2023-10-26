e2c041173923b26f990335862f66525664c3b6eb
Piotr Krusinski <piotr.krusinski@timebook.net>
2023-10-26 10:22:26 +0200

    Updated docker logging method

--- entrypoint.bash
+++ entrypoint.bash
@@ -2,59 +2,63 @@
 set -e
 
 # lets decode SA key and save it locally in key file
-echo -e "Preparing key used for authentication"
+echo -e "[ACTION] Preparing key used for authentication"
 echo "$INPUT_SERVICE_ACCOUNT_KEY" | base64 -d > "$HOME"/sa_key.json
 
 # if no tag is specified use latest, else add latest to the tags anyway
-echo -e "Preparing tag list"
+echo -e "[ACTION] Preparing tag list"
 if [ -z "$INPUT_TAG_LIST" ]; then
   TAG_LIST=(latest)
 else
   TAG_LIST=($INPUT_TAG_LIST latest)
 fi
-echo -e "  TAG_LIST: ${TAG_LIST}"
+echo -e "[ACTION]   TAG_LIST: ${TAG_LIST}"
 
 #  add build args to the command 
-echo -e "Preparing build args"
+echo -e "[ACTION] Preparing build args"
 if [ "$INPUT_BUILD_ARGS" ]; then
   BUILD_ARGS_SPLIT=$(echo "$INPUT_BUILD_ARGS" | tr ',' '\n')
   BUILD_ARGS="--build-arg $(echo $BUILD_ARGS_SPLIT | xargs | sed 's/ / --build-arg /g')"
 fi
-echo -e "  BUILD_ARGS: ${BUILD_ARGS}"
+echo -e "[ACTION]   BUILD_ARGS: ${BUILD_ARGS}"
 
 # authenticate to google using key file
-echo -e "Authenticating to gcloud"
+echo -e "[ACTION] Authenticating to gcloud"
 gcloud auth activate-service-account --key-file="$HOME"/sa_key.json --project "$INPUT_GOOGLE_PROJECT_ID"
 
 # configure docker with specific Artifact Registry
-echo -e "Gcloud Docker configuration"
-gcloud auth configure-docker $INPUT_GOOGLE_ARTIFACT_REGISTRY_HOSTNAME
+#echo -e "[ACTION] Gcloud Docker configuration"
+#gcloud auth configure-docker $INPUT_GOOGLE_ARTIFACT_REGISTRY_HOSTNAME
 
-echo -e "Preparing tags"
+# configure docker with specific Artifact Registry
+echo -e "[ACTION] docker configuration"
+docker login -u _json_key --password-stdin https://$INPUT_GOOGLE_ARTIFACT_REGISTRY_HOSTNAME < ~/.gcloud-api-key.json
+
+echo -e "[ACTION] Preparing tags"
 # check tags and create a docker command parameter
 BUILD_TAGS=""
 for TAG in ${TAG_LIST[@]}; do
-  echo -e "  found tag: ${TAG}"
+  echo -e "[ACTION]   found tag: ${TAG}"
   BUILD_TAGS=" ${BUILD_TAGS} -t $INPUT_IMAGE_NAME:$TAG"
 done
-echo -e "  BUILD_TAGS: ${BUILD_TAGS}"
+echo -e "[ACTION]   BUILD_TAGS: ${BUILD_TAGS}"
 
-echo -e "Building docker image"
+echo -e "[ACTION] Building docker image"
 # build docker image
 docker build -f "$INPUT_DOCKERFILE" $BUILD_TAGS $BUILD_ARGS .
 
-echo -e "Tagging images"
+echo -e "[ACTION] Tagging images"
 # tag docker images
 for TAG in ${TAG_LIST[@]}; do
   IMAGE_TAG=$INPUT_GOOGLE_ARTIFACT_REGISTRY_HOSTNAME/$INPUT_GOOGLE_PROJECT_ID/$INPUT_GOOGLE_ARTIFACT_REGISTRY_NAME/$INPUT_IMAGE_NAME:$TAG
-  echo -e "  tag: ${IMAGE_TAG}"
+  echo -e "[ACTION]   tag: ${IMAGE_TAG}"
   docker tag ${TAG} ${IMAGE_TAG}
 done
 
-echo -e "Pushing images to docker registry"
+echo -e "[ACTION] Pushing images to docker registry"
 # push created docker image with specified tags
 for TAG in ${TAG_LIST[@]}; do
   IMAGE_TAG=$INPUT_GOOGLE_ARTIFACT_REGISTRY_HOSTNAME/$INPUT_GOOGLE_PROJECT_ID/$INPUT_GOOGLE_ARTIFACT_REGISTRY_NAME/$INPUT_IMAGE_NAME:$TAG
-  echo -e "  pushing: ${IMAGE_TAG}"
+  echo -e "[ACTION]   pushing: ${IMAGE_TAG}"
   docker push $INPUT_GOOGLE_ARTIFACT_REGISTRY_HOSTNAME/$INPUT_GOOGLE_PROJECT_ID/$INPUT_GOOGLE_ARTIFACT_REGISTRY_NAME/$INPUT_IMAGE_NAME:$TAG
 done
